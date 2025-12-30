2b1b54e90e0647f4eb25cec4aa63da605fe693cb
Seriki yakub <sandraisom00@gmail.com>
2025-12-30 14:07:52 -0600

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,7 @@
-git clone https://github.com/Web4application/Aura.radio.git
-cd Aura.radio
-docker compose up
-docker run -d --name icecast -p 8000:8000 infiniteproject/icecast
+$ git clone https://github.com/Web4application/Aura.radio.git
+$ cd Aura.radio
+$ docker compose up
+$ docker run -d --name icecast -p 8000:8000 infiniteproject/icecast
+$ git clone https://gitlab.servus.at/aura/tank
+$ cd tank
+$ make

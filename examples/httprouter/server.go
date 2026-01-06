709c593656fe55a0caf7dfca49f7ab20adfd57db
Seriki yakub <sandraisom00@gmail.com>
2026-01-06 16:46:37 +0100

    Update server.go

--- examples/httprouter/server.go
+++ examples/httprouter/server.go
@@ -20,3 +20,13 @@ func main() {
 
 	http.ListenAndServe(":8080", handler)
 }
+
+c := cors.New(cors.Options{
+    AllowedOrigins: []string{"http://foo.com", "http://foo.com:8080"},
+    AllowCredentials: true,
+    // Enable Debugging for testing, consider disabling in production
+    Debug: true,
+})
+
+// Insert the middleware
+handler = c.Handler(handler)
